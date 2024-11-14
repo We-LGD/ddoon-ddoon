13081d584fe8080e1d7e4ba88a32b7f111e7ad2f
parkjooyoung <mirkduddl@naver.com>
2024-11-15 01:30:17 +0900

    chore: color theme 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ export default {
         sub: ['Gaegu', 'cursive'],
         display: ['인천교육자람', 'sans-serif'],
       },
+      colors: {
+        main: '#748D70',
+        active: '#455C3F',
+        highlight: '#0B4203',
+        disabled: '#D9D9D9',
+        disabledHover: '#999999',
+      },
     },
   },
   plugins: [],
