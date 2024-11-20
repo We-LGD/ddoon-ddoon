ef0cf131336a75bd43d9b6bfcbdd150f093a9377
parkjooyoung <mirkduddl@naver.com>
2024-11-21 01:36:00 +0900

    feat: 기본 Route 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import Component from '@/shared/components/Component';
+import { Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Main from '@pages/Main';
+import Login from '@pages/Login';
+import NicknameSetup from '@pages/NicknameSetup';
+import Tutorial from '@pages/Tutorial';
+import Gool from '@pages/Gool';
+import Challenge from '@pages/Challenge';
+import Trip from '@pages/Trip';
+import NotFound from '@pages/NotFound';
+import Loading from '@pages/Loading';
 
 function App() {
   return (
-    <>
-      <Component />
-    </>
+    <Router>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/nickname-setup" element={<NicknameSetup />} />
+          <Route path="/tutorial" element={<Tutorial />} />
+          <Route path="/ddoon-ddoon-gool" element={<Gool />} />
+          <Route path="/challenge" element={<Challenge />} />
+          <Route path="/ddoon-ddoon-trip" element={<Trip />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
