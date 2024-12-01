9a7ba3433309ef9590e18861e7451cbf8aaa9cf5
KIMGEUNDU <vlkoklv@naver.com>
2024-12-01 10:27:35 +0900

    feat: Button 컴포넌트 작업

---
+++ src/shared/components/atoms/Button.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+function Button() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className={`w-[10rem] h-[3.125rem] text-white rounded bg-main hover:bg-active disabled:bg-disabled ${isClicked ? 'bg-active' : 'bg-main'}`}
+      // disabled
+    >
+      Button
+    </button>
+  );
+}
+
+export default Button;
