1098eef3015d7878e3deb2018f4ff53996911939
KIMGEUNDU <vlkoklv@naver.com>
2024-12-01 12:35:10 +0900

    feat: 공통 컴포넌트 Slide 작업

---
+++ src/shared/components/atoms/ChellengeAddBtn.tsx
@@ -0,0 +1,14 @@
+import { FiPlus } from 'react-icons/fi';
+
+function ChellengeAddBtn() {
+  return (
+    <button
+      onClick={() => alert('추가!')}
+      className="w-[28.75rem] h-[3.438rem] rounded-lg bg-gray-200 text-active flex justify-center items-center mt-4"
+    >
+      <FiPlus size={24} />
+    </button>
+  );
+}
+
+export default ChellengeAddBtn;
