4c72d6d5478e0618423fe852ba130653ebe90ab3
parkjooyoung <mirkduddl@naver.com>
2024-12-03 01:13:28 +0900

    fix: store함수 화살표 함수로 수정

--- src/store/useInputStore.ts
+++ src/store/useInputStore.ts
@@ -1,15 +1,13 @@
 import { create } from 'zustand';
 import { InputStore } from '@store/storeType';
 
-function useInputStore() {
-  return create<InputStore>((set) => ({
-    inputs: {},
-    setInput: (name, value) =>
-      set((state) => ({
-        inputs: { ...state.inputs, [name]: value },
-      })),
-    resetInputs: () => set({ inputs: {} }),
-  }))();
-}
+const useInputStore = create<InputStore>((set) => ({
+  inputs: {},
+  setInput: (name, value) =>
+    set((state) => ({
+      inputs: { ...state.inputs, [name]: value },
+    })),
+  resetInputs: () => set({ inputs: {} }),
+}));
 
 export default useInputStore;
