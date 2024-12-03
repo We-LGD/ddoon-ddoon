baa4c5ad7aa88277d353edfe934658791e0e6ad4
KIMGEUNDU <vlkoklv@naver.com>
2024-12-03 22:47:05 +0900

    style: Button 컴포넌트 전역상태를 props 기반 상태로 수정

--- src/shared/components/atoms/Button.tsx
+++ src/shared/components/atoms/Button.tsx
@@ -1,10 +1,10 @@
 import { ButtonProps } from '@/shared/interface/atomsType';
 
-function Button({ name, cancel }: ButtonProps) {
+function Button({ name, cancel, event }: ButtonProps) {
   return (
     <button
-      // onClick={setClick}
-      className={`w-[10rem] h-[3.125rem] text-white rounded hover:bg-active ${cancel ? 'bg-disabled' : ''} ${click ? 'bg-active' : 'bg-main'}`}
+      onClick={event}
+      className={`w-[10rem] h-[3.125rem] text-white rounded hover:bg-active ${cancel ? 'bg-disabled' : 'bg-main'}`}
     >
       {name}
     </button>
