f2e4f38404400cf708a01e417b9329d56c64775c
KIMGEUNDU <vlkoklv@naver.com>
2024-12-03 21:50:39 +0900

    style: 이벤트함수명 수정 및 ChallengeBox props 추가

--- src/shared/components/organisms/ChallengeBox.tsx
+++ src/shared/components/organisms/ChallengeBox.tsx
@@ -1,29 +1,30 @@
 import { useState } from 'react';
 import CloseBtn from '@/shared/components/atoms/CloseBtn';
+import { ChallengeProps } from '@/shared/interface/atomsType';
 
-function ChallengeBox() {
-  const [fail, setFail] = useState(false);
+function ChallengeBox({ title, memo, day, fail }: ChallengeProps) {
+  const [open, setOpen] = useState(false);
 
-  const failEvent = () => {
-    setFail(!fail);
+  const handleOpen = () => {
+    setOpen(!open);
   };
 
   return (
     <>
       <div
-        onClick={failEvent}
-        className={`max-w-[28.75rem] h-[7rem] rounded-lg flex space-x-10 justify-between items-center p-4 relative ${fail ? 'bg-disabled text-gray-400' : 'hover:bg-active group bg-main text-white'}`}
+        onClick={handleOpen}
+        className={`w-[28.75rem] h-[7rem] rounded-lg flex space-x-10 justify-between items-center p-4 relative ${fail ? 'bg-disabled text-gray-400' : 'hover:bg-active group bg-main text-white'}`}
       >
-        <section className="flex flex-col space-y-2 overflow-hidden">
-          <h1 className="text-[1.125rem] font-semibold overflow-hidden text-ellipsis whitespace-nowrap">
-            매일 책 10분 읽기 30일
+        <section className="flex flex-col space-y-2 overflow-hidden w-full">
+          <h1 className="text-[1.125rem] font-semibold overflow-hidden text-ellipsis whitespace-nowrap w-full">
+            {title}
           </h1>
-          <h2 className="text-[0.875rem] overflow-hidden text-ellipsis whitespace-nowrap">화이팅!</h2>
+          <h2 className="text-[0.875rem] overflow-hidden text-ellipsis whitespace-nowrap w-full">{memo}</h2>
         </section>
         <p
           className={`text-active text-[1.125rem] font-bold whitespace-nowrap ${fail ? 'text-gray-400' : 'group-hover:text-main'}`}
         >
-          D-20
+          D-{day}
         </p>
         <CloseBtn isFail={fail} />
       </div>
