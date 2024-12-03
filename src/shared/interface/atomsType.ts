e64f093e9299a7c11a18be89ae79a89c54adf6c0
KIMGEUNDU <vlkoklv@naver.com>
2024-12-03 21:51:58 +0900

    style: interface 위치 수정

--- src/shared/interface/atomsType.ts
+++ src/shared/interface/atomsType.ts
@@ -13,7 +13,7 @@ export interface InputProps {
   maxLength: number;
 }
 export interface FailProps {
-  isFail?: boolean;
+  isFail: boolean;
 }
 
 export interface TitleProps {
@@ -23,11 +23,4 @@ export interface TitleProps {
 export interface ButtonProps {
   name: string;
   cancel?: true;
-}
-
-export interface ChallengeProps {
-  title: string;
-  memo: string;
-  day: number;
-  fail?: true;
 }
