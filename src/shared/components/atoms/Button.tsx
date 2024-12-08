653f13f0d77933be8e681a0f3cba078659bb77ed
KIMGEUNDU <vlkoklv@naver.com>
2024-12-08 21:39:12 +0900

    style: button type 추가

--- src/shared/components/atoms/Button.tsx
+++ src/shared/components/atoms/Button.tsx
@@ -1,10 +1,11 @@
 import { ButtonProps } from '@/shared/interface/atomsType';
 
-function Button({ children, cancel, event }: ButtonProps) {
+function Button({ children, cancel, event, type = 'button' }: ButtonProps) {
   return (
     <button
       onClick={event}
-      className={`w-[10rem] h-[3.125rem] leading-[3.8] text-white rounded hover:bg-active ${cancel ? 'bg-disabled' : 'bg-main'}`}
+      type={type}
+      className={`w-[10rem] h-[3.125rem] leading-[3.8] text-white rounded ${cancel ? 'bg-disabled hover:bg-disabledHover' : 'bg-main hover:bg-active'}`}
     >
       {children}
     </button>
