6493f99120f700b64482b58a2287d7ad7a742593
parkjooyoung <mirkduddl@naver.com>
2024-12-10 21:07:42 +0900

    feat: 튜토리얼 완료 시 뚠뚠굴 페이지 이동 후 보상 팝업이 열리도록 작업

--- src/pages/Gool.tsx
+++ src/pages/Gool.tsx
@@ -1,4 +1,53 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import Swal from 'sweetalert2';
+import Button from '@/shared/components/atoms/Button';
+
 function Gool() {
+  const { state } = useLocation();
+
+  const openTutorialRewardModal = () => {
+    Swal.fire({
+      title: '튜토리얼 성공!',
+      html: `
+        <div>
+          <p>성공 보상으로 굴 하나를 열어줄께</p>
+          <div class="flex justify-center mt-4">
+            <div id="customConfirmButton"></div>
+          </div>
+        </div>
+      `,
+      showConfirmButton: false,
+      showCancelButton: false,
+      customClass: {
+        popup: 'w-[25.375rem] h-[13.625rem] pt-5 font-default text-sm',
+      },
+      didOpen: () => {
+        const confirmButtonContainer = document.getElementById('customConfirmButton');
+
+        if (confirmButtonContainer) {
+          const root = createRoot(confirmButtonContainer);
+          root.render(
+            <Button
+              event={() => {
+                Swal.close();
+              }}
+            >
+              보상받기
+            </Button>,
+          );
+        }
+      },
+    });
+  };
+
+  useEffect(() => {
+    if (state?.tutorialCompleted) {
+      openTutorialRewardModal();
+    }
+  }, [state]);
+
   return <div>Gool</div>;
 }
 
