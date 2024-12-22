cc2716bc9f5a91ad456d95f694cda90f9aeb084e
KIMGEUNDU <vlkoklv@naver.com>
2024-12-22 21:34:54 +0900

    chore: 반응형을 위한 코드 수정

--- src/shared/components/atoms/ChallengeAddBtn.tsx
+++ src/shared/components/atoms/ChallengeAddBtn.tsx
@@ -5,7 +5,7 @@ function ChallengeAddBtn({ event }: { event: MouseEventHandler }) {
   return (
     <button
       onClick={event}
-      className="w-[28.75rem] h-[3.438rem] rounded-lg bg-gray-200 text-active flex justify-center items-center mt-4"
+      className="max-w-[28.75rem] w-full h-[3.438rem] rounded-lg bg-gray-200 text-active flex justify-center items-center mt-4"
     >
       <FiPlus size={24} />
     </button>
