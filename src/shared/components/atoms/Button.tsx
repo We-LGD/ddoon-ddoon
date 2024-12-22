a790fb632f64f73ddca7187ed0409f765610f410
KIMGEUNDU <vlkoklv@naver.com>
2024-12-22 21:32:31 +0900

    chore: 반응형을 위한 코드수정

--- src/shared/components/atoms/Button.tsx
+++ src/shared/components/atoms/Button.tsx
@@ -5,7 +5,7 @@ function Button({ children, cancel, event, type = 'button' }: ButtonProps) {
     <button
       onClick={event}
       type={type}
-      className={`w-[10rem] h-[3.125rem] leading-[3.8] text-white rounded ${cancel ? 'bg-disabled hover:bg-disabledHover' : 'bg-main hover:bg-active'}`}
+      className={`w-full max-w-[10rem] h-[3.125rem] leading-[3.8] text-white rounded ${cancel ? 'bg-disabled hover:bg-disabledHover' : 'bg-main hover:bg-active'}`}
     >
       {children}
     </button>
