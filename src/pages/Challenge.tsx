bc6ceb0146cdd1882745b1adbb4ba2d68189d6e2
parkjooyoung <mirkduddl@naver.com>
2025-01-06 00:38:57 +0900

    fix: Title 컴포넌트 margin-top 추가 + 챌린지 리스트 스크롤 추가

--- src/pages/Challenge.tsx
+++ src/pages/Challenge.tsx
@@ -7,6 +7,7 @@ import useNewChallengeStore from '@/store/useNewChallengeStore';
 import ChallengeBox from '@/shared/components/organisms/ChallengeBox';
 import ChellengeAddBtn from '@/shared/components/atoms/ChallengeAddBtn';
 import ChallengeAddForm from '@/shared/components/templates/ChallengeAddForm';
+import Title from '@/shared/components/atoms/Title';
 
 function Challenge() {
   const MySwal = withReactContent(Swal);
@@ -52,28 +53,27 @@ function Challenge() {
   };
 
   return (
-    <>
-      <div className="flex flex-col items-center relative pb-[4.375rem] px-5">
-        <h1 className="text-lg font-bold my-7">뚠뚠 챌린지</h1>
-        <section className="flex flex-col gap-4 max-w-[28.75rem] w-full">
-          <p className="text-right text-base">{dummy.length} / 10</p>
-          {dummy.map((v, i) => {
-            return (
-              <ChallengeBox
-                key={i}
-                title={v.title}
-                memo={v.memo}
-                day={v.day}
-                index={v.idx}
-                result={v.result}
-                successCheck={v.successCheck}
-              />
-            );
-          })}
-        </section>
-        <ChellengeAddBtn event={handleChallengeAddModal} />
-      </div>
-    </>
+    <div className="flex flex-col items-center relative pb-[4.375rem] px-5 max-h-screen">
+      <Title>뚠뚠 챌린지</Title>
+
+      <p className="max-w-[28.75rem] w-full mb-2 text-right text-base">{dummy.length} / 10</p>
+      <section className="flex flex-col gap-4 max-w-[28.75rem] w-full max-h-screen overflow-y-auto">
+        {dummy.map((v, i) => {
+          return (
+            <ChallengeBox
+              key={i}
+              title={v.title}
+              memo={v.memo}
+              day={v.day}
+              index={v.idx}
+              result={v.result}
+              successCheck={v.successCheck}
+            />
+          );
+        })}
+      </section>
+      <ChellengeAddBtn event={handleChallengeAddModal} />
+    </div>
   );
 }
 
