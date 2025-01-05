c2fb48de4471a8621bc9526c694824bb3063686d
parkjooyoung <mirkduddl@naver.com>
2025-01-06 00:38:57 +0900

    fix: Title 컴포넌트 margin-top 추가 + 챌린지 리스트 스크롤 추가

--- src/shared/components/atoms/Title.tsx
+++ src/shared/components/atoms/Title.tsx
@@ -1,7 +1,7 @@
 import { TitleProps } from '@/shared/interface/atomsType';
 
 function Title({ children }: TitleProps) {
-  return <h1 className="text-[1.125rem] font-bold mb-4">{children}</h1>;
+  return <h1 className="text-[1.125rem] font-bold mt-4 mb-4">{children}</h1>;
 }
 
 export default Title;
