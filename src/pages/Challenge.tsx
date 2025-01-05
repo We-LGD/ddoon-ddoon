2ddb6bfdecfe7ebbe7ed275d12f78bfb9015660f
parkjooyoung <mirkduddl@naver.com>
2025-01-06 00:13:32 +0900

    feat: 챌린지 추가하기 모달의 모든 데이터가 선택된 후 도전하기 버튼 활성화

--- src/pages/Challenge.tsx
+++ src/pages/Challenge.tsx
@@ -3,39 +3,17 @@ import withReactContent from 'sweetalert2-react-content';
 import useDummyStore from '@/store/useDummyStore';
 import useInputStore from '@/store/useInputStore';
 import useSelectDayStore from '@/store/useSelectDayStore';
-import Button from '@/shared/components/atoms/Button';
-import Input from '@/shared/components/atoms/Input';
-import SelectDay from '@/shared/components/atoms/SelectDay';
-import ChellengeAddBtn from '@/shared/components/atoms/ChallengeAddBtn';
-import ChallengeBox from '@/shared/components/organisms/ChallengeBox';
 import useNewChallengeStore from '@/store/useNewChallengeStore';
-import useToolTipStore from '@/store/useTooltipStore';
+import ChallengeBox from '@/shared/components/organisms/ChallengeBox';
+import ChellengeAddBtn from '@/shared/components/atoms/ChallengeAddBtn';
+import ChallengeAddForm from '@/shared/components/templates/ChallengeAddForm';
 
 function Challenge() {
   const MySwal = withReactContent(Swal);
-  const { dummy, setDummy } = useDummyStore();
+  const { dummy } = useDummyStore();
   const { resetInputs } = useInputStore();
-  const { setNewChallenge } = useNewChallengeStore();
   const { setSelect } = useSelectDayStore();
-  const { toolTip, setToolTip } = useToolTipStore();
-
-  const handleFormSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    const currentInputs = useInputStore.getState().inputs;
-    const currentSelect = useSelectDayStore.getState().select;
-
-    setNewChallenge({ ...currentInputs });
-
-    if (!currentSelect) {
-      setToolTip(true);
-    } else {
-      setDummy(useNewChallengeStore.getState().newChallenge);
-      setSelect(null);
-      resetInputs();
-      MySwal.close();
-      window.scrollTo(0, document.body.scrollHeight);
-    }
-  };
+  const { setNewChallenge } = useNewChallengeStore();
 
   const handleChallengeAddModal = () => {
     if (dummy.length >= 10) {
@@ -53,45 +31,14 @@ function Challenge() {
     } else {
       MySwal.fire({
         title: '',
-        html: (
-          <form onSubmit={handleFormSubmit} className="w-full flex flex-col items-center py-10">
-            <fieldset className="w-full flex flex-col justify-center items-center">
-              <Input title="챌린지 제목을 작성해주세요" placeholder="매일 책 10분 읽기" name="title" maxLength={20} />
-              <Input title="다짐을 작성해주세요" placeholder="한 달에 한 권은 끝내보자" name="memo" maxLength={20} />
-            </fieldset>
-            <fieldset className="w-full max-w-[20rem] flex flex-col justify-center items-center relative">
-              <legend className="mb-[0.75rem] text-center">도전 일수를 선택해주세요</legend>
-              <span
-                className={`absolute w-[13.75rem] bottom-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 bg-zinc-800 text-white py-1 rounded-md ${toolTip ? 'animate-tooltip' : 'opacity-0'}`}
-              >
-                도전 일수를 선택해주세요.
-              </span>
-              <SelectDay />
-            </fieldset>
-            <fieldset className="w-full max-w-[20rem] flex justify-between items-center gap-3 mt-5">
-              <Button
-                cancel={true}
-                event={() => {
-                  Swal.close();
-                  setSelect(null);
-                  setNewChallenge({ day: undefined });
-                }}
-              >
-                취소
-              </Button>
-              <Button type="submit">도전 시작</Button>
-            </fieldset>
-          </form>
-        ),
+        html: <ChallengeAddForm />,
         allowOutsideClick: () => {
           setSelect(null);
-          setToolTip(false);
           setNewChallenge({ day: undefined });
           return true;
         },
         allowEscapeKey: () => {
           setSelect(null);
-          setToolTip(false);
           setNewChallenge({ day: undefined });
           return true;
         },
