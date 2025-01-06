8fd26bad0571e7f4a0a63591feb0b97c9b9f54d7
parkjooyoung <mirkduddl@naver.com>
2025-01-07 00:45:21 +0900

    chore: 파일 경로 절대경로로 수정

--- src/store/useSelectDayStore.ts
+++ src/store/useSelectDayStore.ts
@@ -1,13 +1,13 @@
 import { create } from 'zustand';
-import { SelectDayProps } from './storeType';
+import { SelectDayProps } from '@store/storeType';
 
 const useSelectDayStore = create<SelectDayProps>((set) => ({
   select: null,
   setSelect: (day) => {
     set(() => ({
-      select: day
+      select: day,
     }));
-  }
+  },
 }));
 
 export default useSelectDayStore;
