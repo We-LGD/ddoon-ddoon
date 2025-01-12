31dd504d1a41cba6c94a35e403da892c4912521e
parkjooyoung <mirkduddl@naver.com>
2025-01-13 00:45:00 +0900

    feat: Trip 배경 이미지 작업+ 챌린지 간판 select 작업
    - 이미지 사이즈만큼 Height를 맞추기 위해 화면 크기 변경 시 이미지 비율에
      맞춰서 높이 계산 (handleResize)
    - 첫 화면 로드 시 스크롤을 맨 아래로 이동
    - 현재 챌린지 파악을 위한 챌린지 간판 작업
      - 챌린지 간판을 누르면 챌린지 변경이 가능한 List가 나오도록 작업
    - 오늘 첫 방문인지를 파악하기 위해 localStorage에 첫 방문 시 오늘 날짜 저장

--- src/pages/Trip.tsx
+++ src/pages/Trip.tsx
@@ -1,5 +1,130 @@
+import { useEffect, useRef, useState } from 'react';
+import { Images } from '@/shared/assets/images';
+import { IoMdArrowDropup, IoMdArrowDropdown } from 'react-icons/io';
+import SuccessCheckModal from '@/shared/components/organisms/SuccessCheckModal';
+import { challenges } from '@/shared/data/challenges';
+
 function Trip() {
-  return <div>Trip</div>;
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [imageDimensions, setImageDimensions] = useState({ width: 0, height: 0 });
+  const [initialLoad, setInitialLoad] = useState(true);
+  const [challengeSelectShow, setChallengeSelectShow] = useState(false);
+  const [CurrentName, setCurrentCallenge] = useState(challenges[0].name);
+  const [CurrentId, setCurrentId] = useState(challenges[0].idx);
+
+  const handleResize = () => {
+    if (containerRef.current) {
+      const aspectRatio = imageDimensions.height / imageDimensions.width;
+      const containerWidth = containerRef.current.offsetWidth;
+      const adjustedHeight = containerWidth * aspectRatio;
+      setImageDimensions({ width: containerWidth, height: adjustedHeight });
+    }
+  };
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = Images.구름맵30일;
+    img.onload = () => {
+      if (containerRef.current) {
+        const aspectRatio = img.height / img.width;
+        const containerWidth = containerRef.current.offsetWidth;
+        const adjustedHeight = containerWidth * aspectRatio;
+        setImageDimensions({ width: containerWidth, height: adjustedHeight });
+
+        if (initialLoad) {
+          setInitialLoad(false);
+          setTimeout(() => {
+            if (containerRef.current) {
+              containerRef.current.scrollTop = containerRef.current.scrollHeight;
+            }
+          }, 0);
+        }
+      }
+    };
+  }, [initialLoad]);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [imageDimensions]);
+
+  useEffect(() => {
+    const lastVisitedDate = localStorage.getItem(`challenge_${CurrentId}_lastVisitedDate`);
+    const today = new Date().toISOString().split('T')[0];
+
+    if (!lastVisitedDate || lastVisitedDate !== today) {
+      SuccessCheckModal();
+      localStorage.setItem(`challenge_${CurrentId}_lastVisitedDate`, today);
+    }
+  }, [CurrentId]);
+
+  return (
+    <div ref={containerRef} className="w-fill h-[calc(100vh-54px)] overflow-y-auto">
+      <div
+        className="relative w-full"
+        style={{
+          height: `${imageDimensions.height}px`,
+          backgroundImage: `url(${Images.구름맵30일})`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'bottom',
+          zIndex: 10,
+        }}
+      >
+        <div
+          className="relative w-full"
+          style={{
+            backgroundImage: `url(${Images.통나무})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'bottom',
+            zIndex: 10,
+          }}
+        ></div>
+
+        <div
+          className="fixed top-[1%] left-1/2 transform -translate-x-1/2 w-[14rem] h-[4rem] z-30 flex items-center justify-center cursor-pointer"
+          style={{
+            backgroundImage: `url(${Images.통나무})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'contain',
+          }}
+          onClick={() => setChallengeSelectShow(!challengeSelectShow)}
+        >
+          <p className="wood-sign">{CurrentName}</p>
+          {challengeSelectShow ? (
+            <>
+              <IoMdArrowDropup className="wood-sign" />
+              <div className="absolute top-[105%] left-1/2 transform -translate-x-1/2 bg-white w-[14rem] shadow-lg rounded-lg overflow-hidden">
+                {challenges.map((challenge) => (
+                  <p
+                    id={challenge.idx}
+                    className="hover:bg-main hover:text-white transition-all  py-2 px-4"
+                    onClick={() => {
+                      setCurrentCallenge(challenge.name);
+                      setCurrentId(challenge.idx);
+                    }}
+                  >
+                    {challenge.name}
+                  </p>
+                ))}
+              </div>
+            </>
+          ) : (
+            <IoMdArrowDropdown className="wood-sign" />
+          )}
+        </div>
+
+        <img
+          src={Images.기본뚠뚠}
+          alt="기본뚠뚠"
+          className="absolute bottom-[3%] left-1/2 transform -translate-x-1/2 w-[12%] object-contain"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Trip;
---
+++ src/shared/data/challenges.ts
@@ -0,0 +1,26 @@
+export const challenges = [
+  {
+    idx: '1',
+    name: '챌린지 1',
+    days: 30,
+    successCount: 3,
+  },
+  {
+    idx: '2',
+    name: '챌린지 2',
+    days: 20,
+    successCount: 5,
+  },
+  {
+    idx: '3',
+    name: '챌린지 3',
+    days: 10,
+    successCount: 7,
+  },
+  {
+    idx: '4',
+    name: '챌린지 4',
+    days: 15,
+    successCount: 10,
+  },
+];
