5001210480e2770ada8dbb7f80ec989b023b7e3e
parkjooyoung <mirkduddl@naver.com>
2025-01-13 00:27:36 +0900

    feat: 챌린지 완료 확인 모달 작업

---
+++ src/shared/components/organisms/SuccessCheckModal.tsx
@@ -0,0 +1,43 @@
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import Button from '@/shared/components/atoms/Button';
+
+function SuccessCheckModal() {
+  const ReactSwal = withReactContent(Swal);
+
+  ReactSwal.fire({
+    icon: 'question',
+    html: (
+      <>
+        <div className="font-default text-sm">
+          <p className="mb-4">오늘의 챌린지를 완료 하셨나요?</p>
+          <section className="flex justify-center items-center gap-3">
+            <Button
+              theme="modal"
+              cancel={true}
+              event={() => {
+                Swal.close();
+              }}
+            >
+              취소
+            </Button>
+            <Button
+              theme="modal"
+              event={() => {
+                Swal.close();
+              }}
+            >
+              확인
+            </Button>
+          </section>
+        </div>
+      </>
+    ),
+    showConfirmButton: false,
+    customClass: {
+      popup: 'max-w-[25.375rem]',
+    },
+  });
+}
+
+export default SuccessCheckModal;
