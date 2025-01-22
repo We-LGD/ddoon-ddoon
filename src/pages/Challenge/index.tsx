b8bfa0e30e837ea019e3ca5aedf20354ee728af7
parkjooyoung <mirkduddl@naver.com>
2025-01-22 22:31:22 +0900

    feat: 로그아웃 버튼 작업

--- src/pages/Challenge/index.tsx
+++ src/pages/Challenge/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import useDummyStore from '@/store/useDummyStore';
 import useInputStore from '@/store/useInputStore';
 import useSelectDayStore from '@/store/useSelectDayStore';
@@ -9,6 +10,7 @@ import LimitModal from '@/pages/Challenge/LimitModal';
 import AddModal from '@/pages/Challenge/AddModal';
 
 function Challenge() {
+  const navigate = useNavigate();
   const { dummy } = useDummyStore();
   const { resetInputs } = useInputStore();
   const { setSelect } = useSelectDayStore();
@@ -34,12 +36,22 @@ function Challenge() {
     }
   };
 
-  return (
-    <div className="flex flex-col items-center relative pb-[4.375rem] px-5 max-h-screen">
-      <Title>뚠뚠 챌린지</Title>
+  const handleLogout = () => {
+    navigate('/');
+    resetInputs();
+  };
 
-      <p className="max-w-[28.75rem] w-full mb-2 text-right text-base">{dummy.length} / 10</p>
-      <section className="flex flex-col gap-4 max-w-[28.75rem] w-full max-h-screen overflow-y-auto">
+  return (
+    <div className="flex flex-col items-center relative pb-[4.375rem] px-10 max-h-screen">
+      <div className="relative w-full text-center">
+        <Title>뚠뚠 챌린지</Title>
+        <button onClick={handleLogout} className="absolute top-5 right-0 text-disabledHover">
+          로그아웃
+        </button>
+      </div>
+
+      <p className="w-full mb-2 text-right text-base">{dummy.length} / 10</p>
+      <section className="flex flex-col gap-4 w-full max-h-screen overflow-y-auto">
         {dummy.map((v, i) => {
           return (
             <ChallengeBox
