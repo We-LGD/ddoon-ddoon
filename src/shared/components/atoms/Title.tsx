755fdb3288e26f950344635a1da3b551b761b412
parkjooyoung <mirkduddl@naver.com>
2025-01-29 02:17:36 +0900

    feat: ToolTip 컴포넌트 작업 및 적용
    - Gool 컴포넌트 실패 이미지 hover 시 챌린지 명 확인을 위해 ToolTip이 나오도록 작업
    - Trip에서 WoodSign hover 시 챌린지명 전체를 확인하기 위해 ToolTip이 나오도록 작업

--- src/shared/components/atoms/Title.tsx
+++ src/shared/components/atoms/Title.tsx
@@ -1,6 +1,6 @@
-import { TitleProps } from '@/shared/interface/atomsType';
+import { ChildrenProps } from '@/shared/interface/atomsType';
 
-function Title({ children }: TitleProps) {
+function Title({ children }: ChildrenProps) {
   return <h1 className="text-[1.125rem] font-bold mt-4 mb-4">{children}</h1>;
 }
 
