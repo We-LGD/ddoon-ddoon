96c33d3b895485f2febcdaa960228339a01d9584
parkjooyoung <mirkduddl@naver.com>
2025-03-06 02:19:00 +0900

    feat: 뚠뚠굴 페이지 firebase(email, github)로그인 시 닉네임 나오도록 작업

--- frontend/src/pages/Gool/Background.tsx
+++ frontend/src/pages/Gool/Background.tsx
@@ -1,12 +1,30 @@
 import { useEffect, useState } from 'react';
+import { getAuth } from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
 import { Images } from '@/shared/assets/images';
 import { LayoutProps } from '@/shared/interface/templatesType';
 import TutorialRewardModal from '@/pages/Gool/TutorialRewardModal';
+import { db } from '@/shared/utils/firebase';
 
 export default function Background({ children }: LayoutProps) {
   const [width, setWidth] = useState(0);
+  const [nickname, setNickname] = useState<string>('');
 
   useEffect(() => {
+    const getNickname = async () => {
+      const user = getAuth().currentUser;
+      if (user) {
+        const userRef = doc(db, 'users', user.uid);
+        const userSnap = await getDoc(userRef);
+
+        if (userSnap.exists()) {
+          setNickname(userSnap.data().nickname || '갓생뚠뚠');
+        }
+      }
+    };
+
+    getNickname();
+
     const tutorialModal = localStorage.getItem('tutorialModal');
     if (tutorialModal === 'true') {
       TutorialRewardModal();
@@ -37,7 +55,7 @@ export default function Background({ children }: LayoutProps) {
           backgroundSize: 'contain',
         }}
       >
-        <p className="wood-sign">갓생뚠뚠굴</p>
+        <p className="wood-sign">{nickname}굴</p>
       </div>
 
       <div
