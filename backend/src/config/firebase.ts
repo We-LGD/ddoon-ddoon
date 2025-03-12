782495c1bd30e4ca7d24d654f09aca30258799dc
parkjooyoung <mirkduddl@naver.com>
2025-03-12 22:10:55 +0900

    feat: Nodejs Firebase Admin SDK 초기화 및 Firestore, Auth 연결

---
+++ backend/src/config/firebase.ts
@@ -0,0 +1,17 @@
+import * as admin from "firebase-admin";
+import * as path from "path";
+
+// Firebase Admin SDK 초기화
+if (admin.apps.length === 0) {
+  admin.initializeApp({
+    credential: admin.credential.cert(
+      require(path.join(__dirname, "firebaseServiceAccountKey.json"))
+    ),
+  });
+}
+
+// Firebase Admin 객체들 (Firestore, Auth 등)
+const db = admin.firestore();
+const auth = admin.auth();
+
+export { db, auth };
