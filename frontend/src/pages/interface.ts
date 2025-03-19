63f2cbd222a5a02748188a2bc16b6662d4527b7d
parkjooyoung <mirkduddl@naver.com>
2025-03-19 22:03:50 +0900

    remove: Dummy Store 파일 삭제

--- frontend/src/pages/interface.ts
+++ frontend/src/pages/interface.ts
@@ -29,11 +29,3 @@ export interface NewChallengeProps {
   result: 'progress';
   successCheck: boolean;
 }
-
-//TODO - 서버 연결시 삭제하기
-export interface DummyProps {
-  dummy: ChallengeProps[];
-  setDummy: (challenge: ChallengeProps) => void;
-  deleteDummy: (index: number) => void;
-  updateDummy: (index: number, result?: 'progress' | 'success' | 'fail', successCheck?: boolean) => void;
-}
