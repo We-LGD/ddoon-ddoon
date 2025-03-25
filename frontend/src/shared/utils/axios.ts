7a72a29678e7f9bb3b948f3cd5c404189e9217fb
parkjooyoung <mirkduddl@naver.com>
2025-03-25 23:12:41 +0900

    feat: Axios Instance 토큰 갱신 로직 추가+자동 로그아웃 처리

--- frontend/src/shared/utils/axios.ts
+++ frontend/src/shared/utils/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
+import { getAuth } from 'firebase/auth';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:3000',
@@ -20,9 +21,24 @@ axiosInstance.interceptors.request.use(
 
 axiosInstance.interceptors.response.use(
   (response) => response,
-  (error) => {
+  async (error) => {
     if (error.response && error.response.status === 401) {
-      //TODO: 자동 로그아웃 처리 + 토큰 갱신 로직 추가
+      try {
+        const auth = getAuth();
+        const user = auth.currentUser;
+
+        if (user) {
+          const newToken = await user.getIdToken(true);
+          Cookies.set('userToken', newToken, { expires: 1 });
+          error.config.headers['Authorization'] = `Bearer ${newToken}`;
+
+          return axiosInstance(error.config);
+        }
+      } catch (refreshError) {
+        console.error('토큰 갱신 실패, 로그아웃 처리', refreshError);
+        Cookies.remove('userToken');
+        window.location.href = '/login';
+      }
     }
     return Promise.reject(error);
   },
