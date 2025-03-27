78cff0bfee1f88fdd06f529143049b24140cc046
parkjooyoung <mirkduddl@naver.com>
2025-03-27 22:21:49 +0900

    feat: 튜토리얼 상태 조회 Api 구현(GET /tutorial-status)

--- backend/src/router/gool.ts
+++ backend/src/router/gool.ts
@@ -5,6 +5,36 @@ import { verifyToken } from "../middleware/authMiddleware";
 const router = express.Router();
 
 /**
+ * [GET] 튜토리얼 상태 가져오기
+ * 사용자가 튜토리얼을 완료했는지 확인
+ */
+router.get(
+  "/tutorial-status",
+  verifyToken,
+  async (req: Request, res: Response) => {
+    try {
+      const userId = (req as any).user.uid;
+
+      // 해당 사용자 문서에서 튜토리얼 완료 여부 확인
+      const userRef = db.collection("users").doc(userId);
+      const userDoc = await userRef.get();
+
+      if (!userDoc.exists) {
+        res.status(404).send("User not found");
+        return;
+      }
+
+      const { tutorialCompleted } = userDoc.data() as any; // 튜토리얼 완료 여부
+
+      res.json({ tutorialCompleted });
+    } catch (error) {
+      console.error("Error fetching tutorial status:", error);
+      res.status(500).send("Error fetching tutorial status");
+    }
+  }
+);
+
+/**
  * [GET] Gool 리스트 가져오기
  */
 router.get("/gool-list", verifyToken, async (req: Request, res: Response) => {
