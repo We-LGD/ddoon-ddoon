28b53d3c0175f18d06bfe66c1d282ef2252960d8
parkjooyoung <mirkduddl@naver.com>
2025-03-29 01:07:41 +0900

    refactor: 로그인 상태 확인 조건 변경 (localStorage -> Auth Token)

--- frontend/src/shared/utils/logout.ts
+++ frontend/src/shared/utils/logout.ts
@@ -3,15 +3,8 @@ import { auth } from '@/shared/utils/firebase';
 
 const logout = async () => {
   try {
-    // 카카오 로그아웃
     await axios.post('http://localhost:3000/auth/logout', {}, { withCredentials: true });
-    localStorage.removeItem('isKaKaoLoggedIn');
-
-    // 이메일 로그아웃
     await auth.signOut();
-
-    // 게스트 로그아웃
-    localStorage.removeItem('isLoggedIn');
   } catch (error) {
     console.error('로그아웃 실패:', error);
   }
