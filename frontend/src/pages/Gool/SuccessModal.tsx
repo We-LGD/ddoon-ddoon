63079aa009b6e471082477eb5abd376d2bc6c5e2
parkjooyoung <mirkduddl@naver.com>
2025-03-28 19:00:47 +0900

    feat: Gool 페이지 Gool Api 연결

--- frontend/src/pages/Gool/SuccessModal.tsx
+++ frontend/src/pages/Gool/SuccessModal.tsx
@@ -11,14 +11,16 @@ export default function SuccessModal({ title, imageSrc, onClose }: SuccessModalP
     html: (
       <div className="relative bg-white text-center">
         <Title>{title}</Title>
-        <img src={imageSrc} alt="Success" className="w-full h-auto" />
-        <img src={Images.성공도장} alt="Success" className="absolute bottom-6 right-6 bg-no-repeat w-[25%]" />
-        <p
-          className="absolute bottom-10 left-10 font-display text-4xl font-bold transform rotate-12"
-          style={{ textShadow: '1px 1px 0px #FFFFFF' }}
-        >
-          갓생시 뚠뚠로 30Day
-        </p>
+        <div className="w-full h-full bg-slate-600">
+          <img src={imageSrc} alt="Success" className="w-full h-full" />
+          <img src={Images.성공도장} alt="Success" className="absolute bottom-6 right-6 bg-no-repeat w-[25%]" />
+          <p
+            className="absolute bottom-10 left-10 font-display text-4xl font-bold transform rotate-12"
+            style={{ textShadow: '1px 1px 0px #FFFFFF' }}
+          >
+            갓생시 뚠뚠로 30Day
+          </p>
+        </div>
       </div>
     ),
     showConfirmButton: false,
